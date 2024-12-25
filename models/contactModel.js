06f451576b3c0cb39b79b924df423a4a9df2c686
Ramakrishna T <147640669+RamakrishnaTanam@users.noreply.github.com>
2024-12-25 23:03:51 +0530

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -9,3 +9,15 @@ const contactSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+ },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
